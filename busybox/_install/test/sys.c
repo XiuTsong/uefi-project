ec50f59e606a0cdcfc14182c987fdf1da57fbe73
lucky6six <1537016727@qq.com>
2023-12-27 18:23:47 +0800

    fix:make initramfs

---
+++ busybox/_install/test/sys.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SYSCALL 332
+int main(int argc, char *argv[]) {
+  syscall(SYSCALL);
+  printf("ok! run dmesg | grep runtime in terminal!\n");
+  return 0;
+}
---
+++ test/sys.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define SYSCALL 332
+int main(int argc, char *argv[]) {
+  syscall(SYSCALL);
+  printf("ok! run dmesg | grep runtime in terminal!\n");
+  return 0;
+}
